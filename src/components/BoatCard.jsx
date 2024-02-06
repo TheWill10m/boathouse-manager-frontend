d6d3c5b426b825cb6d01c54499d915e3f68e1c4c
Will Dalgleish <dev@wdalgleish.com>
2024-02-06 21:22:35 +0000

    feat: boat card delete button functionality
    - delete function uses context to remove boat from list
    
    - listId added as component prop to remove boat from list by index

--- src/components/BoatCard.jsx
+++ src/components/BoatCard.jsx
@@ -1,7 +1,16 @@
+import { BoatContext } from '../App.jsx';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
 
 function BoatCard(props) {
+
+    const { boatList, setBoatList } = useContext(BoatContext);
+
+    function deleteBoat(index) {
+        const updatedBoatList = boatList.filter((_, i) => i !== index)
+        setBoatList(updatedBoatList);
+    }
 
     return (
         <Card style={{ width: '18rem' }}>
@@ -17,7 +26,11 @@ function BoatCard(props) {
                             <Button variant='outline-primary'>Edit</Button>
                         </Col>
                         <Col>
-                            <Button variant='outline-danger' placement='right'>Delete</Button>
+                            <Button
+                                variant='outline-danger'
+                                placement='right'
+                                onClick={() => deleteBoat(props.listId)}
+                            >Delete</Button>
                         </Col>
                     </Row>
                 </Container>
