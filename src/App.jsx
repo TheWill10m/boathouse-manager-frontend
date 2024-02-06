289dc639761e744bc3b0d6bafc7b05957922b866
Will Dalgleish <dev@wdalgleish.com>
2024-02-06 11:58:11 +0000

    feat: add new boat form
    - has input for all information currently displayed in boat cards
    
    - boat type selection changes based on whether it is coxed
    
    - submit button has no functionality yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import BoatsExplorer from './components/BoatsExplorer.jsx'
 import Header from './components/Header.jsx'
+import NewBoatForm from './components/NewBoatForm.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <NewBoatForm />
       <BoatsExplorer />
     </>
   )
---
+++ src/components/NewBoatForm.jsx
@@ -0,0 +1,68 @@
+import { Button, Container, Form, InputGroup } from 'react-bootstrap';
+import { useState } from 'react';
+
+function NewBoatForm() {
+
+    const isCoxedDefault = true;
+    const inServiceDefault = true;
+
+    const [isCoxed, setIsCoxed] = useState(isCoxedDefault)
+    const [inService, setInService] = useState(inServiceDefault)
+
+    function handleIsCoxedChange(event) {
+        setIsCoxed(event.target.checked)
+    }
+
+    function handleInServiceChange(event) {
+        setInService(event.target.checked)
+    }
+
+    function handleSubmit(event) {
+
+    }
+
+    function getBoatTypes() {
+        let boatTypes = isCoxed ? ['2+', '4+', '8x+', '8+'] : ['1x', '2x', '2-', '4x', '4-']
+        return boatTypes;
+    }
+
+    return (
+        <Container style={{ padding: '10px', border: '2px solid hsl(0, 0%, 20%)', borderRadius: '10px', marginTop: '1%', marginBottom: '5px', width: '50%' }}>
+            <Form>
+                <Form.Label><h3>Add a new boat</h3></Form.Label>
+                <InputGroup>
+                    <InputGroup.Text>Boat name</InputGroup.Text>
+                    <Form.Control type='text' placeholder='Enter boat name' />
+                </InputGroup>
+
+                <Form.Group controlId='formCoxed' style={{ marginTop: '5px' }}>
+                    <Form.Check type='checkbox' id='isCoxed' label='Is the boat coxed?' checked={isCoxed} onChange={handleIsCoxedChange} />
+                </Form.Group>
+
+                <Form.Group controlId='formBoatType' style={{ marginTop: '5px' }}>
+                    <Form.Label>What type of boat is it?</Form.Label>
+                    <Form.Select aria-label='Default select example'>
+                        <option>Select boat type</option>
+                        {getBoatTypes().map((boatType, index) => (
+                            <option key={index} id={boatType}>{boatType}</option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+
+                <Form.Group controlId='formBoatStatus' style={{ marginTop: '10px' }}>
+                    <Form.Label>Is the boat in service?</Form.Label>
+                    <Form.Check type='checkbox' id='inService' label='In service' checked={inService} onChange={handleInServiceChange} />
+                </Form.Group>
+
+                <Button variant='primary' onSubmit={handleSubmit} style={{ margin: '5px' }}>
+                    Submit
+                </Button>
+            </Form >
+        </Container>
+    );
+
+
+
+}
+
+export default NewBoatForm;
